a39ce480391e78335604a42cb895a76cb9557b6b
yen31b <yen.badilla@gmail.com>
2020-03-02 22:55:09 -0600

    Pruebas de comunicación cliente-servidor

---
+++ TE1/include/GraphNode.h
@@ -0,0 +1,46 @@
+#ifndef _CGRAPHNODE_H_
+#define	_CGRAPHNODE_H_
+
+#include <vector>
+#include <AlgebraLib.h>
+
+class CGraph;
+
+class CGraphNode
+{
+public:
+    using id_type = unsigned int;
+    CGraphNode( id_type id, CGraph* pGraph );
+    CGraphNode( const CGraphNode& node ) = default;
+    CGraphNode& operator=( const CGraphNode& node ) = default;
+    bool Visited();
+    void SetParent( CGraphNode* parent );
+    void SetVisited( bool visit );
+    void SetPosition( const Mat4D::CVector4D& pos );
+    void SetName( std::string name );
+    void SetGraph( CGraph* graph );
+    void SetWeight( float fWeight );
+    void SetBlocked( bool bBlocked );
+    void SetTentativeDistance( float fTentativeDistance );
+    Mat4D::CVector4D GetPosition();
+    CGraphNode* GetParent();
+    id_type     GetID();
+    std::string GetName();
+    CGraph*     GetGraph();
+    float       GetWeight();
+    float       GetTentativeDistance();
+    bool        IsBlocked();
+    ~CGraphNode();
+private:
+    id_type		m_dwID;
+    float       m_fTentativeDistance;
+    float       m_fWeight;
+    bool		m_bVisited;
+    bool        m_bBlocked;
+    CGraph* m_pGraph;
+    std::string m_name;
+    CGraphNode*	m_pParent;
+    Mat4D::CVector4D m_position;
+};
+
+#endif //_CGRAPHNODE_H_
---
+++ TE1/src/DijkstraWalker.cpp
@@ -0,0 +1,76 @@
+#include "DijkstraWalker.h"
+#include "Graph.h"
+
+CDijkstraWalker::CDijkstraWalker()
+{
+
+}
+
+CDijkstraWalker::~CDijkstraWalker()
+{
+
+}
+
+bool CDijkstraWalker::Step()
+{
+    if ( m_pEnd && m_pCurrent &&
+         m_iCurrentSteps++ < m_iMaxSteps && !m_nodes.empty() )
+    {
+        m_pCurrent = *m_nodes.begin();
+        m_nodes.erase( m_nodes.begin() );
+
+        if ( m_pCurrent != m_pEnd )
+        {
+            m_pCurrent->SetVisited( true );
+            LoadConnections( m_pCurrent );
+        }
+        else
+            CreatePath( m_pCurrent );
+    }
+    else
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void CDijkstraWalker::LoadConnections( CGraphNode *pNode )
+{
+    auto connections = std::move( GetConnections( pNode ) );
+
+    for ( auto& node : connections )
+    {
+        float fTentDist = pNode->GetTentativeDistance() + node->GetWeight();
+        float fNodeTent = node->GetTentativeDistance();
+        if ( !node->Visited() && !node->IsBlocked() &&
+             fTentDist < fNodeTent )
+        {
+            auto it = m_nodes.find( node );
+
+            if ( it != m_nodes.end() )
+            {
+                m_nodes.erase( it );
+            }
+
+            node->SetTentativeDistance( fTentDist );
+
+            m_nodes.insert( node );
+
+            node->SetParent( pNode );
+        }
+    }
+}
+
+void CDijkstraWalker::ClearNodes()
+{
+    m_nodes.clear();
+}
+
+
+/*******Clase Para Comprar pesos******/
+
+bool CLessWeight::operator()( CGraphNode *lhs, CGraphNode *rhs ) const
+{
+    return lhs->GetTentativeDistance() < rhs->GetTentativeDistance();
+}
